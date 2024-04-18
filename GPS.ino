b5481303310100de3d31219e2bbc3dec503b4392
Sunera <120769726+SuneraUdana@users.noreply.github.com>
2024-04-19 01:42:14 +0530

    Update GPS.ino

--- GPS.ino
+++ GPS.ino
@@ -1,26 +1,26 @@
 #include <TinyGPS++.h> //Libary TinyGPS
-#include <SoftwareSerial.h> //Libarary bawaan
+#include <SoftwareSerial.h> //Libarary softwareserial
 #include <Ultrasonic.h>
 // Choose two Arduino pins to use for software serial
-int RXPin = 2; //Connect ke TX GPS
-int TXPin = 3; //Connect ke RX GPS
+int RXPin = 2; //Connect  TX GPS
+int TXPin = 3; //Connect  RX GPS
 Ultrasonic ultrasonic(7, 8);
 int distance;
-int GPSBaud = 9600; //Biarin default
+int GPSBaud = 9600; //default baud
 long duration;
 const int buzzerPin=10;
 
-// Membuat objek TinyGPS++
+// Creating TinyGPS++ objects
 TinyGPSPlus gps;
 
-// Mmebuat koneksi serial dengan nama "gpsSerial"
+// Create a serial connection with the  "gpsSerial"
 SoftwareSerial gpsSerial(RXPin, TXPin);
 
 void setup()
 {
-  //Memulai koneksi serial pada baudrate 9600
+  //Starting a serial connection on baudrate 9600
   Serial.begin(9600);
-  //Memulai koneksi serial dengan sensor
+  //Starting a serial connection with a sensor 
   pinMode(buzzerPin,OUTPUT);
   gpsSerial.begin(GPSBaud);
 
@@ -28,7 +28,7 @@ void setup()
 
 void loop()
 {
-  //Membuat tampilan data ketika terdapat koneksi
+  //Creating a data display when there is a connection 
   while (gpsSerial.available() > 0){
     if (gps.encode(gpsSerial.read())){
       obs();
@@ -39,7 +39,7 @@ void loop()
     }
   }
 
-  // Jika dalam 5 detik tidak ada koneksi, maka akan muncul error "No GPS detected"
+  // if within 5 seconds there is no connection, an error "No GPS detected" will appear.
 }
 void obs(){
   distance = ultrasonic.read();
